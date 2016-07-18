8605c1fb24753d6b39f744585755b1dd1c466311
martin <martin@10.0.1.7>
2016-07-18 16:06:37 +0800

    remove snippet and update classpath

--- src/mabit/Snippet.java
+++ src/mabit/Snippet.java
@@ -2,11 +2,13 @@ package mabit;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Snippet {
 	public static void main(String[] s) {
@@ -14,11 +16,17 @@ public class Snippet {
 		try {
 			url = new URL("http://bitcoincharts.com/markets/");
 
-			URLConnection con = url.openConnection();
-			InputStream in = con.getInputStream();
-			String encoding = con.getContentEncoding();
-			encoding = encoding == null ? "UTF-8" : encoding;
-			String body = IOUtils.toString(in, encoding);
+//			URLConnection con = url.openConnection();
+//			InputStream in = con.getInputStream();
+//			String encoding = con.getContentEncoding();
+//			encoding = encoding == null ? "UTF-8" : encoding;
+//			String body = IOUtils.toString(in, encoding);
+//			Document doc = Jsoup.parse(body);
+			Document doc = Jsoup.connect(url.toString()).get();
+			String body = doc.toString();
+			PrintWriter writter = new PrintWriter("/Users/martin/Documents/dev/mabit/market.html");
+			writter.print(body);
+			writter.close();
 			System.out.println(body);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
