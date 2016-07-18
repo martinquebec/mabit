26f0e4fb1a75194c3c2a24aa58fcd8d90a2efca7
martin <martin@10.0.1.7>
2016-07-18 19:16:03 +0800

    now saves a csv

--- src/mabit/data/BitCoinCharts.java
+++ src/mabit/data/BitCoinCharts.java
@@ -4,7 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Currency;
+import java.util.List;
+import java.util.Vector;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,7 +17,9 @@ import mabit.utils.XNumber;
 public class BitCoinCharts {
 	final public static String URL = "http://bitcoincharts.com/markets/";
 	final public static String LOCAL_FILE = "/Users/martin/Documents/dev/mabit/market.html";
+	final public static String LOCAL_CSV = "/Users/martin/Documents/dev/mabit/marketStats.csv";
 	final public static String CLOUD_FILE = "";
+	List<BccRow> allRows;
 //~~~~~~~~~~~~~~~~~~~~~~ BccRow
 	public static class BccRow {
 		String name;
@@ -184,36 +187,19 @@ public class BitCoinCharts {
 	}
 	
 	public void parse() throws IOException {
-		/*
-  <tr id="okcoinCNY" currency="CNY" market="OKCoin" class=" CNY" timestamp="2c57d7a3df8db513ed31c31be6d10ffb18846c702cd9862274d52715" onclick="window.location='/markets/okcoinCNY.html';return false;"> 
-   <td class="arrow" change="up" latest_trade="1468804824"> <span class="up">▲</span> <span class="sub">CNY</span> </td> 
-   <td class="symbol"> 
-    <nobr> 
-     <a href="/markets/okcoinCNY.html">OKCoin</a> 
-    </nobr> <span class="sub"><a href="/markets/okcoinCNY.html">okcoinCNY</a></span> </td> 
-   <td>4553.4 <span class="sub">0 min ago</span> </td> 
-   <td class="minichart break"> <span volume="40027.94,144193.901,235598.212,235818.443,241671.122,227394.089,227736.292,217823.027,220756.287,167927.997,186155.57,176085.043,171491.197,161447.797,179665.212,186737.186,157871.174,544737.705,1569196.314,991147.235,1160071.71,878259.744,729446.772,833851.179,716203.202,698259.276,633180.828,447324.348,669275.546,35895.12" price="5092.95406773,5012.81615921,4566.7287103,4302.08909481,3862.43412903,4286.04264586,4429.92713232,4226.2051012,4216.2139042,4311.12826412,4231.98171804,4379.98287857,4530.94646062,4631.4674707,4513.91399692,4483.42953127,4518.1905641,4534.11190911,4289.40063881,4338.20363148,4351.09663549,4336.59715305,4357.8555222,4412.9039174,4441.61122365,4400.38021589,4446.06110558,4439.29238097,4500.80015532,4553.77460048" avg="4386.649565481277685641445808" class="marketsparkline"></span> </td> 
-   <td>4386.65 <span class="sub change">166.75 3.80%</span> </td> 
-   <td>13,085,249.47 <span class="sub">57,400,403,893.02 CNY</span> </td> 
-   <td><span class="split">3550</span> <span class="split">5134.1</span> </td> 
-   <td class="break">4552.26</td> 
-   <td>4552.31</td> 
-   <td class="break">4503.50 <span class="sub change">49.90 1.11%</span> </td> 
-   <td>705,170.67 <span class="sub">3,175,733,767.13 CNY</span> </td> 
-   <td> <span class="split">4428.58</span> <span class="split">4594.97</span> </td> 
-  </tr> 
-
-		 */
+		
 		Element marketsTable = doc.getElementById("markets");
 		Element marketsBody = marketsTable.getElementsByTag("tbody").first();
 		Elements rows = marketsBody.getElementsByTag("tr");
-		System.out.println("There are " + rows.size() + "rows");
+		allRows= new Vector<BccRow>(rows.size());
+	//	System.out.println("There are " + rows.size() + "rows");
 		System.out.println("row#,"+BccRow.getCsvHeader());	
 		int i = 0;
 		for(Element row : rows) {
 			BccRow bccRow = new BccRow(row);
-			System.out.println(""+i+","+ bccRow.toCsvRow());
-			i++;
+			allRows.add(bccRow);
+		//	System.out.println(""+i+","+ bccRow.toCsvRow());
+		//	i++;
 		}
 	//	PrintWriter writter = new PrintWriter("/Users/martin/Documents/dev/mabit/row.html");
 	//	writter.print(row);
@@ -221,19 +207,29 @@ public class BitCoinCharts {
 	}
 
 	
-	public void save(String filename) throws FileNotFoundException {
+	public void saveHtml(String filename) throws FileNotFoundException {
 	PrintWriter writter = new PrintWriter(filename);
 		writter.print(doc); 
 		writter.close();
 
+	}
+	public void saveCSV(String filename) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(filename);
+		writer.println(BccRow.getCsvHeader());
+		for(BccRow row: allRows) {
+			writer.println(row.toCsvRow());	
+		}
+		writer.close();
 	}
 		
 	public static void main(String[] s) {
 //		System.out.println(Double.parseDouble("13,085,249.47".replaceAll(",", "")));
 //		System.exit(0);
 		try {
-			BitCoinCharts bcc = new BitCoinCharts(new File(LOCAL_FILE),URL);
+//			BitCoinCharts bcc = new BitCoinCharts(new File(LOCAL_FILE),URL);
+			BitCoinCharts bcc = new BitCoinCharts(URL);
 			bcc.parse();
+			bcc.saveCSV(LOCAL_CSV);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
